2043c54b6997170bb5c71ab0679f40ece9221f0f
Thomas Müller <thomas.mueller@tmit.eu>
2017-06-28 17:17:19 +0200

    Redirect a download to the direct download url if provided

--- apps/dav/lib/Connector/Sabre/FilesPlugin.php
+++ apps/dav/lib/Connector/Sabre/FilesPlugin.php
@@ -30,7 +30,6 @@
 
 namespace OCA\DAV\Connector\Sabre;
 
-use OC\Files\View;
 use OCP\Files\ForbiddenException;
 use Sabre\DAV\Exception\Forbidden;
 use Sabre\DAV\Exception\NotFound;
@@ -44,8 +43,6 @@ use \Sabre\HTTP\ResponseInterface;
 use OCP\Files\StorageNotAvailableException;
 use OCP\IConfig;
 use OCP\IRequest;
-use Sabre\DAV\Exception\BadRequest;
-use OCA\DAV\Connector\Sabre\Directory;
 use OCA\DAV\Upload\FutureFile;
 
 class FilesPlugin extends ServerPlugin {
@@ -84,11 +81,6 @@ class FilesPlugin extends ServerPlugin {
 	 * @var bool
 	 */
 	private $isPublic;
-
-	/**
-	 * @var View
-	 */
-	private $fileView;
 
 	/**
 	 * @var bool
@@ -159,6 +151,7 @@ class FilesPlugin extends ServerPlugin {
 		$this->server->on('afterBind', [$this, 'sendFileIdHeader']);
 		$this->server->on('afterWriteContent', [$this, 'sendFileIdHeader']);
 		$this->server->on('afterMethod:GET', [$this,'httpGet']);
+		$this->server->on('method:GET', [$this,'redirect']);
 		$this->server->on('afterMethod:GET', [$this, 'handleDownloadToken']);
 		$this->server->on('afterResponse', function($request, ResponseInterface $response) {
 			$body = $response->getBody();
@@ -224,6 +217,22 @@ class FilesPlugin extends ServerPlugin {
 		}
 	}
 
+	function redirect(RequestInterface $request, ResponseInterface $response) {
+		// Only handle valid files
+		$node = $this->tree->getNodeForPath($request->getPath());
+		if (!($node instanceof File)) {
+			return;
+		}
+
+		$redirect = $node->getDirectDownload();
+		if (is_string($redirect) && $redirect !== '') {
+			$response->setHeader('Location', $redirect);
+			$response->setStatus(302);
+			return false;
+		}
+
+		return;
+	}
 	/**
 	 * Add headers to file download
 	 *
